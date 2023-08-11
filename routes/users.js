40c5a3ba0181e4559b88b267d34f60958dbd7f03
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-08-11 23:17:50 +0530

    created users array and a post method

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,35 @@
 import express from 'express';
-
-
-
 const router = express.Router();
+
+const users = [
+    {
+        firstName: "Gaurav",
+        lastName: "Sarage",
+        age: 20
+    },
+    {
+        firstName: "Aryan",
+        lastName: "Sarage",
+        age: 13
+    }
+]
+
 
 // all routes here are starting with /users
 router.get('/', (req, res) => {
-    res.send('Hello');
+    console.log(users);
+    res.send(users);
 });
+
+
+router.post('/', (req, res) => {
+    console.log('POST ROUTE REACHED');
+
+    users.push();
+
+    res.send('POST ROUTE REACHED');
+})
+
+
 
 export default router;
